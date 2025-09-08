95c0fdf956917eeb6ead55e5000efb8f546add30
Your Name <Kadeem.daniels3tech@gmail.com>
2025-09-08 08:34:53 -0600

    added a basemap and plotted the points for the ISS.

--- ISS_now.R
+++ ISS_now.R
@@ -2,8 +2,21 @@ install.packages("httr2")
 library(httr2)
 install.packages("jsonlite")
 library(jsonlite)
+install.packages("leaflet")
+library(leaflet)
 
 iss <- request("http://api.open-notify.org/iss-now.json")
 issNow <- iss |> req_perform()
 dataISS <- issNow |> resp_body_json()
 print(dataISS)
+
+#getting the lat and long from our data
+lat <- as.numeric(dataISS$iss_position$latitude)
+long <- as.numeric(dataISS$iss_position$longitude)
+
+#elements we need to create the map
+
+leaflet() |> 
+  addTiles()|>
+  addMarkers(lng = long, lat = lat, popup = "The ISS is here!")  |>
+  setView(lng = long, lat = lat,zoom = 3)
